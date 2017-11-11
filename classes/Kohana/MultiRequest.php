8d84b4bb7437cbf53e1e09b587ce8e10aaedb9ef
Andrey Tkachenko <tkachenko.dn@inbox.ru>
2017-11-11 18:22:34 +0300

    Формирование опций curl и ответа позаимствованы из Request_Client_Curl::_send_message

--- classes/Kohana/MultiRequest.php
+++ classes/Kohana/MultiRequest.php
@@ -3,101 +3,50 @@
 class Kohana_MultiRequest {
 
 	/**
-	 * @var resource curl_init
-	 */
-	private $ch;
-	/**
 	 * @var resource curl_multi_init
 	 */
 	private $cm;
-	private $channels = [];
+	private $handles = [];
 
 	public function __construct($options = [])
 	{
-		$this->ch = curl_init();
 		$this->cm = curl_multi_init();
 
-		curl_setopt_array($this->ch, $options);
+		foreach ($options as $option => $value)
+		{
+			if ( ! curl_multi_setopt($this->cm, $option, $value))
+			{
+				throw new Request_Exception('Failed to set CURL options, check CURL documentation: :url',
+					[':url' => 'http://php.net/manual/en/function.curl-multi-setopt.php']);
+			}
+		}
 	}
 
 	public function __destruct()
 	{
 		curl_multi_close($this->cm);
-		curl_close($this->ch);
 	}
 
 
 	public function add(Request $request)
 	{
 		/**
-		 * @var $client Request_Client_External
+		 * @var $client Request_Client_Curl
 		 */
 		$client = $request->client();
 
 		if ( ! ($client instanceof Request_Client_Curl))
 		{
-			throw new Request_Exception('Request client must be Request_Client_Curl');
+			throw new Curl_Exception('Client must be Request_Client_Curl');
 		}
 
-		$copy_handle = curl_copy_handle($this->ch);
-//		$copy_handle = curl_init();
+		$curl = curl_init();
 
-		curl_setopt_array($copy_handle, $client->options());
-
-		if ( ! UTF8::is_ascii($request->uri()))
-		{
-			$request->uri(curl_escape($copy_handle, $request->uri()));
-		}
-
-		curl_setopt($copy_handle, CURLOPT_URL, $request->uri());
-
-		if (curl_multi_add_handle($this->cm, $copy_handle) !== 0)
-		{
-			// Доступно с PHP 7 >= 7.1.0
-			throw new Request_Exception(curl_multi_errno($this->cm));
-		}
-
-		$this->channels[] = $copy_handle;
-
-		// :TODO: закрытие дескриптора?
-	}
-
-	public function execute()
-	{
-		// execute - if there is an active connection then keep looping
-//		$active = NULL;
-
-		/*do
-		{
-			$status = curl_multi_exec($this->cm, $active);
-		}
-		while ($active AND $status == CURLM_OK);*/
-
-		do
-		{
-			curl_multi_exec($this->cm, $running);
-			curl_multi_select($this->cm);
-		}
-		while ($running > 0);
-
-		// echo the content, remove the handlers, then close them
-		foreach ($this->channels as $chan)
-		{
-			Minion_CLI::write(substr(curl_multi_getcontent($chan), 0, 50));
-			curl_multi_remove_handle($this->cm, $chan);
-			curl_close($chan);
-		}
-	}
-/*
-	private function _send_message(Request $request, $curl)
-	{
-		// Response headers
-		$response_headers = array();
-
-		$options = array();
+		/* =========================================================================== */
+		$options = [];
 
 		// Set the request method
-		$options = $this->_set_curl_request_method($request, $options);
+		$options = $client->_set_curl_request_method($request, $options);
 
 		// Set the request body. This is perfectly legal in CURL even
 		// if using a request other than POST. PUT does support this method
@@ -127,15 +76,16 @@ class Kohana_MultiRequest {
 			$options[CURLOPT_COOKIE] = http_build_query($cookies, NULL, '; ');
 		}
 
+//		$response = Response::factory();
 		// Get any exisiting response headers
-		$response_header = $response->headers();
+//		$response_header = $response->headers();
 
 		// Implement the standard parsing parameters
-		$options[CURLOPT_HEADERFUNCTION]        = array($response_header, 'parse_header_string');
+//		$options[CURLOPT_HEADERFUNCTION]        = array($response_header, 'parse_header_string');
 
-		$client_options = $request->client()->options();
+		$client_options = $client->options();
 		$client_options[CURLOPT_RETURNTRANSFER] = TRUE;
-		$client_options[CURLOPT_HEADER]         = FALSE;
+		$client_options[CURLOPT_HEADER]         = TRUE;
 
 		// Apply any additional options set to
 		$options += $client_options;
@@ -146,52 +96,84 @@ class Kohana_MultiRequest {
 		{
 			$uri .= '?'.http_build_query($query, NULL, '&');
 		}
-
-		// Open a new remote connection
-//		$curl = curl_init($uri);
+		/* =========================================================================== */
 
 		// Set connection options
 		if ( ! curl_setopt_array($curl, $options))
 		{
 			throw new Request_Exception('Failed to set CURL options, check CURL documentation: :url',
-				array(':url' => 'http://php.net/curl_setopt_array'));
+				[':url' => 'http://php.net/manual/en/function.curl-setopt-array.php']);
 		}
 
-		// Get the response body
-//		$body = curl_exec($curl);
-
-		// Get the response information
-		$code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-		if ($body === FALSE)
+		if ( ! UTF8::is_ascii($request->uri()))
 		{
-			$error = curl_error($curl);
+			$request->uri(curl_escape($curl, $request->uri()));
 		}
 
-		// Close the connection
-//		curl_close($curl);
+		curl_setopt($curl, CURLOPT_URL, $uri);
 
-		if (isset($error))
+		if (curl_multi_add_handle($this->cm, $curl) !== 0)
 		{
-			throw new Request_Exception('Error fetching remote :url [ status :code ] :error',
-				array(':url' => $request->url(), ':code' => $code, ':error' => $error));
+			// Доступно с PHP 7 >= 7.1.0
+			throw new Curl_Exception(curl_multi_errno($this->cm));
 		}
 
-		$response->status($code)->body($body);
+		$this->handles[] = $curl;
+	}
 
-		return $curl;
-	}*/
-
-	/*private function _set_curl_request_method(Request $request, array $options)
+	public function execute(callable $f)
 	{
-		switch ($request->method()) {
-			case Request::POST:
-				$options[CURLOPT_POST] = TRUE;
-				break;
-			default:
-				$options[CURLOPT_CUSTOMREQUEST] = $request->method();
-				break;
+		// execute - if there is an active connection then keep looping
+//		$active = NULL;
+
+		/*do
+		{
+			$status = curl_multi_exec($this->cm, $active);
 		}
-		return $options;
-	}*/
+		while ($active AND $status == CURLM_OK);*/
+
+		do
+		{
+			curl_multi_exec($this->cm, $running);
+			curl_multi_select($this->cm);
+		}
+		while ($running > 0);
+
+		// echo the content, remove the handlers, then close them
+		foreach ($this->handles as $h)
+		{
+			$response = new Response();
+
+			$body = curl_multi_getcontent($h);
+
+			// Get the response information
+			$code        = curl_getinfo($h, CURLINFO_HTTP_CODE);
+			$header_size = curl_getinfo($h, CURLINFO_HEADER_SIZE);
+
+			if ($body === FALSE)
+			{
+				$error = curl_error($h);
+			}
+
+			if (isset($error))
+			{
+				throw new Request_Exception('Error fetching remote :url [ status :code ] :error',
+					array(':url' => curl_getinfo($h, CURLINFO_EFFECTIVE_URL), ':code' => $code, ':error' => $error));
+			}
+
+			$response->status($code);
+			$response->body(substr($body, $header_size));
+			$response->protocol(substr($body, 0, 8));
+
+			/**
+			 * @var $headers HTTP_Header
+			 */
+			$headers = $response->headers();
+			$headers->parse_header_string(NULL, substr($body, 0, $header_size));
+
+			call_user_func($f, $response);
+			curl_multi_remove_handle($this->cm, $h);
+			curl_close($h);
+		}
+	}
 }
